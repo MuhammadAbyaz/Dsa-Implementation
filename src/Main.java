4b863c100916d794d55c720efb794fded7e51362
Muhammad Abyaz <MuhammadAbyaz@users.noreply.github.com>
2023-10-13 21:55:44 +0500

    added multiple methods in doubly linked list

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
-import list.DoubleLinkedList;
+import list.DoublyLinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        DoubleLinkedList<Integer> doubleLinkedList = new DoubleLinkedList<>();
+        DoublyLinkedList<Integer> doubleLinkedList = new DoublyLinkedList<>();
+
+        doubleLinkedList.addLast(5);
+        doubleLinkedList.addLast(4);
+        doubleLinkedList.addLast(3);
+        doubleLinkedList.addLast(2);
+
+        doubleLinkedList.addAt(2, 1);
+        System.out.println(doubleLinkedList.toString());
     }
 }
